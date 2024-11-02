9cf381e8ce3e6e40387154a3da01dca0a89edc53
Tushar <tushar.desai87@gmail.com>
2024-11-02 15:29:07 +0530

    entire code

---
+++ src/Practice/ArrayCode.java
@@ -0,0 +1,14 @@
+package Practice;
+
+public class ArrayCode {
+public static void main(String[] args) {
+	int a []= {10,20,30,40,50,60,70};
+	System.out.println(a.length);
+	for (int i:a)
+	{
+		System.out.print(i+"\t");
+	}
+	
+	
+}
+}
